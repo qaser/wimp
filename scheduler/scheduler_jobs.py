a840e321d84ea75d72c05addcbdb5f049ea1f02f
qaser <huji@mail.ru>
2022-09-27 17:06:42 +0500

    добавлено расписание для заказа техники

--- scheduler/scheduler_jobs.py
+++ scheduler/scheduler_jobs.py
@@ -69,12 +69,22 @@ def scheduler_jobs():
         minute=0,
         timezone=const.TIME_ZONE
     )
+    # напоминание о заказе техники с "пн" по "чт"
     scheduler.add_job(
         send_vehicle_notify,
         'cron',
-        day_of_week='mon-fri',
+        day_of_week='mon-thu',
         hour=16,
-        minute=17,
+        minute=0,
+        timezone=const.TIME_ZONE
+    )
+    # напоминание о заказе техники в "пятницу"
+    scheduler.add_job(
+        send_vehicle_notify,
+        'cron',
+        day_of_week='fri',
+        hour=11,
+        minute=30,
         timezone=const.TIME_ZONE
     )
     # scheduler.add_job(
