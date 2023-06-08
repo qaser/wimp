c4754c6cc193be372636256238df905a679957a1
unknown <huji@mail.ru>
2023-06-08 22:48:40 +0500

    fix

--- telegram_bot.py
+++ telegram_bot.py
@@ -46,7 +46,7 @@ async def start_handler(message: types.Message):
 
 @dp.message_handler()
 async def help_handler(message: types.Message):
-    if message.forum_topic_created:
+    if message.:
         await message.answer('обана')
 
 
@@ -56,7 +56,7 @@ async def all_commands(message: types.Message):
 
 
 # удаление сервисного сообщения 'пользователь удалён'
-@dp.message_handler(content_types=['pinned_message', 'left_chat_member'])
+@dp.message_handler(content_types=['pinned_message', 'left_chat_member', 'forum_topic_created', 'forum_topic_closed', 'forum_topic_reopened'])
 async def delete_service_pinned_message(message: types.Message):
     try:
         await bot.delete_message(message.chat.id, message.message_id)
