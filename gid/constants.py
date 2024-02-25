5e4761a844b2045bb65ca0e08a29d19e51142a1e
Alexey S <huji@mail.ru>
2024-02-26 00:32:48 +0500

    gid!!!

--- gid/constants.py
+++ gid/constants.py
@@ -33,7 +33,7 @@ code = '0a92e2ba-d3c5-46ac-a59e-9e902b7ec948  .  9640ff8b-c7ab-4197-91ae-4da0c4e
 import requests
 
 cookies = {
-    'X-CSRF-TOKEN': '345a589d1c287396bf4ba870db8e8ac49a426705c3bf7eeefd424457b5476a25.8d802db0ffa8fa03f0a4a0dd14b58f89faf23cd21a04f19d72b6f77e3f510df58ef30414e635e901f9d7576a0883033e47163adb1d7335d8252e6f400775c1f8c6d9ef9b2481a020a6d7b849452ce991741d09265dba2eee3bcba32495aa651284ce0c8c69e4bd3e43cf20cc06cc75f03a83ec7c6097cbf55824a61a25b416d2a14c1c1527c3943c373f21c218ec2cfb53221feebbb11f695dd6d4f4cec9df514c99bfcb3de5e4136a6d8972dde068765adc738288ad1854f6e4f91be4ce3c9f863019ed749741c6203fee46409677a351c2d228ae82e79dd6747322b9427bb0525d47cb7ff25afbf4f31c77ae423b9f8e58d2ae49c841db8d57177d673b7022',
+    'X-CSRF-TOKEN': '',
 }
 
 headers = {
@@ -44,17 +44,16 @@ headers = {
     'Content-Type': 'application/json; charset=utf-8',
     'Referer': 'https://web.gid.ru/article/cHAcJnu4luia',
     'X-Requested-With': 'XMLHttpRequest',
-    'X-CSRF-TOKEN': '345a589d1c287396bf4ba870db8e8ac49a426705c3bf7eeefd424457b5476a25.8d802db0ffa8fa03f0a4a0dd14b58f89faf23cd21a04f19d72b6f77e3f510df58ef30414e635e901f9d7576a0883033e47163adb1d7335d8252e6f400775c1f8c6d9ef9b2481a020a6d7b849452ce991741d09265dba2eee3bcba32495aa651284ce0c8c69e4bd3e43cf20cc06cc75f03a83ec7c6097cbf55824a61a25b416d2a14c1c1527c3943c373f21c218ec2cfb53221feebbb11f695dd6d4f4cec9df514c99bfcb3de5e4136a6d8972dde068765adc738288ad1854f6e4f91be4ce3c9f863019ed749741c6203fee46409677a351c2d228ae82e79dd6747322b9427bb0525d47cb7ff25afbf4f31c77ae423b9f8e58d2ae49c841db8d57177d673b7022',
-    'sentry-trace': 'b05d0cab19814704a6a4dd65c273f4d5-ab70aef6abd79ca8',
+    'X-CSRF-TOKEN': '',
+    'sentry-trace': '',
     'baggage': 'sentry-environment=production,sentry-public_key=bb91aa88b03040fa9497506d5fa8e028,sentry-trace_id=b05d0cab19814704a6a4dd65c273f4d5',
     'Origin': 'https://web.gid.ru',
     'DNT': '1',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
-    'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJBLWFMMDZBRmduOGxoNDBXakV0bXo0a2owNzdKX3lXWFhIUXJHT2lnSzZVIn0.eyJleHAiOjE3MDg4ODk5NzQsImlhdCI6MTcwODg4ODE3NCwiYXV0aF90aW1lIjoxNzA4ODgxNzc0LCJqdGkiOiJmM2I0MzJjNi1kNTk2LTQ3YmQtODVjOS01ZGVjYWM1MTRjNmMiLCJpc3MiOiJodHRwczovL2FwcC5naWQucnUvYXV0aC9yZWFsbXMvZ2lkIiwic3ViIjoiYTJhZDM4NzMtYmE3YS00YWYzLWExNTQtMzU5N2VlNzMzNzkyIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoid2ViYXBwIiwibm9uY2UiOiI1NGRiMDU3OC0wM2Y0LTQ4MTgtYjFhMC1mNTgwODBhNWY5ODciLCJzZXNzaW9uX3N0YXRlIjoiOTY0MGZmOGItYzdhYi00MTk3LTkxYWUtNGRhMGM0ZWVlMzdlIiwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImVtcGxveWVlIl19LCJzY29wZSI6Im9wZW5pZCIsInNpZCI6Ijk2NDBmZjhiLWM3YWItNDE5Ny05MWFlLTRkYTBjNGVlZTM3ZSJ9.Zpa6cjbRZi7hnMDkKKj_HKrwXxz-NdxCeQxRVRombARkCfNQ7aAVyzeA2A6KuCoOWcB09drxSsotbeosb_k-7n-NhoSpdGYPcBXYX6q7MVaesxaI-mCaM7tFRIJdKI-cWMKwQ3EnZjmDICV7VsAWyxbO8GzkfWnxkvqKFgZCs5uqg_02PaKIQ-dgs2ioJAehlcoRzkaQ8QB0ruX_ieleR_QLFom266zphInwhj1Qc_ZlOIQDhhExSxhLaorsFsFYbYcdIdeC_932m23bSkc_4PC5UP1EQLQMB7UYQGrsVJnXj5pqmx2s_5YMQ238f9YCu3Hi45hp-v9iRM6ObS0_fA',
+    'Authorization': 'Bearer ',
     'Connection': 'keep-alive',
-    # 'Cookie': 'X-CSRF-TOKEN=345a589d1c287396bf4ba870db8e8ac49a426705c3bf7eeefd424457b5476a25.8d802db0ffa8fa03f0a4a0dd14b58f89faf23cd21a04f19d72b6f77e3f510df58ef30414e635e901f9d7576a0883033e47163adb1d7335d8252e6f400775c1f8c6d9ef9b2481a020a6d7b849452ce991741d09265dba2eee3bcba32495aa651284ce0c8c69e4bd3e43cf20cc06cc75f03a83ec7c6097cbf55824a61a25b416d2a14c1c1527c3943c373f21c218ec2cfb53221feebbb11f695dd6d4f4cec9df514c99bfcb3de5e4136a6d8972dde068765adc738288ad1854f6e4f91be4ce3c9f863019ed749741c6203fee46409677a351c2d228ae82e79dd6747322b9427bb0525d47cb7ff25afbf4f31c77ae423b9f8e58d2ae49c841db8d57177d673b7022',
     # Requests doesn't support trailers
     # 'TE': 'trailers',
 }
