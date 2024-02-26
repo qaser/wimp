5ca596f916ccecd788a8ec25d7dad81f61616601
Alexey S <huji@mail.ru>
2024-02-26 16:43:19 +0500

    fix

--- telegram_bot.py
+++ telegram_bot.py
@@ -36,7 +36,7 @@ async def main():
     scheduler.add_job(
         refresh_token_func,
         'interval',
-        minutes=1,
+        minutes=29,
         timezone=const.TIME_ZONE
     )
     scheduler.start()
